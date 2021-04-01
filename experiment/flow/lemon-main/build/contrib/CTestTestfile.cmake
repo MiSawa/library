501f951c6b58a2d152d62da20ce8c242003a2374
mi_sawa <mi.sawa.1216+git@gmail.com>
2021-04-01 22:32:55 +0900

    Remove heaviest directory

--- experiment/flow/lemon-main/build/contrib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/misawa/Dropbox/Program/template/git/experiment/flow/lemon-main/contrib
-# Build directory: /home/misawa/Dropbox/Program/template/git/experiment/flow/lemon-main/build/contrib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- experiment/flow/lemon-main/build/tools/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/misawa/Dropbox/Program/template/git/experiment/flow/lemon-main/tools
-# Build directory: /home/misawa/Dropbox/Program/template/git/experiment/flow/lemon-main/build/tools
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
